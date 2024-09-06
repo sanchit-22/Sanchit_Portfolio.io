a4e477db042b993e5b1e38b910932f5aa88d9172
Sanchit Kumar <kumarsanchit19114@gmail.com>
2024-09-07 02:51:35 +0530

    Portfolio About me and background done

---
+++ script.js
@@ -0,0 +1,34 @@
+let lastScrollTop = 0;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        header.style.top = '-100px';  // Hide header
+    } else {
+        // Scrolling up
+        header.style.top = '0';  // Show header
+    }
+    lastScrollTop = scrollTop;
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('a[href^="#"]'); // Select all links that start with #
+    
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault(); // Prevent default anchor click behavior
+            
+            const targetId = this.getAttribute('href'); // Get the href attribute
+            const targetSection = document.querySelector(targetId); // Find the target section
+            
+            // Scroll to the target section smoothly
+            targetSection.scrollIntoView({
+                behavior: 'smooth' // Smooth scroll
+            });
+        });
+    });
+});
